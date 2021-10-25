8b58a5b03abf5cba1cdc2a60795a273d801f2f38
LingyuLiu11 <linalxly@gmail.com>
2021-10-24 22:32:38 -0400

    add signup page and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   
-  get  'static_pages/home'
-  get  'static_pages/help'
+  get 'users/new'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
   devise_for :users
-  root 'application#hello'
-  # root 'customer/home#home'
+  # root 'application'
+  root 'customer/home#home'
+  # root 'static_pages#home'
   # root 'users#users'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
