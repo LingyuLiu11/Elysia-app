833fa3c435f747c882781e7ef5abcbb641ad8be4
LingyuLiu11 <linalxly@gmail.com>
2021-11-01 22:28:36 -0400

    update user login and autheticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'customer/home#index'
+  #root 'customer/home#index'
   # root 'users#users'
+  get 'sessions/new'
+  get 'users/new'
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
