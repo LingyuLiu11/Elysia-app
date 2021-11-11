70a2f13d4f9baa186a7a505d161fd40877122471
Zhendong Yin <zhendongyin@brandeis.edu>
2021-11-11 14:27:16 -0500

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
   config.serve_static_files = true
   config.assets.digest = true
   # Mount Action Cable outside main process or domain.
