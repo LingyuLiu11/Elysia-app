83ee68d40958a46b9d301908a3cfe9d4f09ffd6c
Zhendong Yin <zhendongyin@brandeis.edu>
2021-11-25 14:09:11 -0500

    fix

--- app/javascript/packs/google_map.js
+++ app/javascript/packs/google_map.js
@@ -1,4 +1,6 @@
 import { Loader } from '@googlemaps/js-api-loader';
+import MarkerClusterer from '@googlemaps/markerclustererplus';
+
 const loader = new Loader({
   apiKey: "AIzaSyAvwMrjYJ-pgIW-gEaxSQFkIqEPvwnNyQI",
   version: "weekly",
@@ -7,26 +9,63 @@ const loader = new Loader({
 
 const mapOptions = {
   center: {
-    lat: 0,
-    lng: 0
+    lat:  42.376,
+    lng:  -71.235
   },
-  zoom: 4
+  zoom: 15
 };
+let map 
+loader.load().then(() => {
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  let infoWindow = new google.maps.InfoWindow();
 
-// Promise
-loader
-  .load()
-  .then((google) => {
-    new google.maps.Map(document.getElementById("map"), mapOptions);
-  })
-  .catch(e => {
-    console.log(123)
+  let datas = JSON.parse($('.store-locations').attr('data'))
+  $.each(datas, function(i, val) {
+    const marker = new google.maps.Marker({
+      position: val,
+      map: map,
+    });
+  });
+
+  // new MarkerClusterer({ markers, map });
+ 
+  const locationButton = document.createElement("button");
+  const script = document.createElement("script");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
 });
 
-loader.loadCallback(e => {
-  if (e) {
-    console.log(e);
-  } else {
-    new google.maps.Map(document.getElementById("map"), mapOptions);
-  }
-});
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
